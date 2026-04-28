d12060bdf6586f48afd705644824b83f528626b3
himansh <himansh@atomicmail.io>
2026-04-28 23:09:31 +0530

    The Boards Are Loading

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # chess
 A simple chess game.
+
+## attribution
+- Chess White Perspective Board: By <a href="//commons.wikimedia.org/wiki/User:CyrilKato" title="User:CyrilKato">CyrilKato</a> - <span class="int-own-work" lang="en">Own work</span>, <a href="http://creativecommons.org/publicdomain/zero/1.0/deed.en" title="Creative Commons Zero, Public Domain Dedication">CC0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=181725083">Link</a>
+- Chess Black Perspective Board: By <a href="//commons.wikimedia.org/wiki/User:CyrilKato" title="User:CyrilKato">CyrilKato</a> - <span class="int-own-work" lang="en">Own work</span>, <a href="http://creativecommons.org/publicdomain/zero/1.0/deed.en" title="Creative Commons Zero, Public Domain Dedication">CC0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=181725072">Link</a>
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,39 @@
+#include "chess_black_perspective_board.h"
+#include "chess_white_perspective_board.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <optional>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Chess");
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Chess");
 
-    while (window.isOpen()) {
-        while (const std::optional event{window.pollEvent()}) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
+  sf::Texture chessBlackPerspectiveBoardTexture(chess_black_perspective_board,
+                                                5231);
+  sf::Texture chessWhitePerspectiveBoardTexture(chess_white_perspective_board,
+                                                5233);
+  sf::Sprite chessBlackPerspectiveBoard(chessBlackPerspectiveBoardTexture);
+  sf::Sprite chessWhitePerspectiveBoard(chessWhitePerspectiveBoardTexture);
 
-        window.clear(sf::Color::Cyan);
-
-        window.display();
-
+  while (window.isOpen()) {
+    while (const std::optional event{window.pollEvent()}) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+    sf::Vector2u windowCenter{window.getSize().x / 2, window.getSize().y / 2};
 
-    return 0;
+    window.clear(sf::Color::Cyan);
+
+    window.draw(chessBlackPerspectiveBoard);
+
+    window.display();
+  }
+
+  return 0;
 }
