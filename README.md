0ebffcb75e07b70029699d08d163ae196b3d2784
himansh <himansh@atomicmail.io>
2026-05-16 08:14:18 +0530

    added logo attribution

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chess
 A simple chess game.
+
+## Attribution:
+- <a href="https://www.flaticon.com/free-icons/chess" title="chess icons">Chess icons created by Freepik - Flaticon</a>
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,33 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <optional>
 
 class WindowManager {
-	private:
-		sf::RenderWindow window{sf::VideoMode{{800,600}},"Chess"};
-	public:
-		sf::RenderWindow& get_window() {
-			return window;
-		}
+ private:
+  sf::RenderWindow window{sf::VideoMode{{800, 600}}, "Chess"};
+
+ public:
+	WindowManager() {
+		window.setIcon(sf::Image::loadFromMemory());
+	}
+  sf::RenderWindow &get_window() { return window; }
 };
 
 int main() {
-	WindowManager windowManager;
-	while (windowManager.get_window().isOpen()) {
-		while (const std::optional event{windowManager.get_window().pollEvent()}) {
-			if (event->is<sf::Event::Closed>()) {
-				windowManager.get_window().close();
-			}
-		}
+  WindowManager windowManager;
+  while (windowManager.get_window().isOpen()) {
+    while (const std::optional event{windowManager.get_window().pollEvent()}) {
+      if (event->is<sf::Event::Closed>()) {
+        windowManager.get_window().close();
+      }
+    }
 
-		windowManager.get_window().clear(sf::Color::Cyan);
+    windowManager.get_window().clear(sf::Color::Cyan);
 
-		windowManager.get_window().display();
-	}
+    windowManager.get_window().display();
+  }
 }
